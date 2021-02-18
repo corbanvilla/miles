9aea0ee529a02097017ab81e688f18ad68cf88b6
Corban Villa <corbanvilla@gmail.com>
2021-02-17 21:41:44 -0700

    incr build #

--- docker/build.sh
+++ docker/build.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-docker build .. -f Dockerfile-index -t animcogn/miles:index-0.1
+docker build .. -f Dockerfile-index -t animcogn/miles:index-0.2
 
-docker build .. -f Dockerfile-predict -t animcogn/miles:predict-0.1
+docker build .. -f Dockerfile-predict -t animcogn/miles:predict-0.2
 
-docker build .. -f Dockerfile-train -t animcogn/miles:train-0.1
+docker build .. -f Dockerfile-train -t animcogn/miles:train-0.2
 
-docker image push animcogn/miles:index-0.1
+docker image push animcogn/miles:index-0.2
 
-docker image push animcogn/miles:predict-0.1
+docker image push animcogn/miles:predict-0.2
 
-docker image push animcogn/miles:train-0.1
+docker image push animcogn/miles:train-0.2
